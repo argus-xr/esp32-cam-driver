e85a4a3a8cced8ee24bfa28a600536d82271021e
Dekker3D <dekker3d@gmail.com>
2020-02-23 22:41:23 +0100

    Ported the project to PlatformIO. No obvious issues.

---
+++ ControllerDriver/src/networkconfig.h
@@ -0,0 +1,9 @@
+#ifndef NETWORKCONFIG_H
+#define NETWORKCONFIG_H
+
+// Copy this file and rename it to networkconfig.h
+
+#define NETSSID "AbandonAllHopeYeWhoEnterHere"
+#define NETPASS "FamilieDekker"
+#define NETHOST "192.168.11.122"
+#endif
