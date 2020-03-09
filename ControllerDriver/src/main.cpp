3723c58cc1b9c8cde6093de45271cd90dc1f1f87
Dekker3D <dekker3d@gmail.com>
2020-03-09 21:52:34 +0100

    All indents changed to 4-space (some were 2-space, and VSCode was just imitating that).

--- ControllerDriver/src/main.cpp
+++ ControllerDriver/src/main.cpp
@@ -5,17 +5,17 @@
 #include "argus-netbuffer/BasicMessageProtocol/BasicMessageProtocol.h"
 
 void setup() {
-  try {
-    delay(2000);
-    Serial.begin(115200);
+    try {
+        delay(2000);
+        Serial.begin(115200);
 
-    NH::startNetworkHandlerTask();
-    CH::startCameraHandlerTask();
-  } catch(std::exception e) {
-    Serial.printf("Exception in setup(): %s", e.what());
-  }
+        NH::startNetworkHandlerTask();
+        CH::startCameraHandlerTask();
+    } catch(std::exception e) {
+        Serial.printf("Exception in setup(): %s", e.what());
+    }
 }
 
 void loop() {
-  delay(1000);
+    delay(1000);
 }
--- ControllerDriver/src/networkhandler.cpp
+++ ControllerDriver/src/networkhandler.cpp
@@ -31,129 +31,129 @@ const TickType_t xBlockTime = pdMS_TO_TICKS( 200 );
 
 
 void handleNetworkStuff() {
-  try {
-    wStatus = WiFi.status();
+    try {
+        wStatus = WiFi.status();
 
-    if (wStatus != WL_CONNECTED && wStatus != WL_IDLE_STATUS) {
-      WiFi.begin(ssid, password);
-      Serial.print("Current status: ");
-      Serial.print(wStatus);
-      Serial.print(". Reconnecting to ");
-      Serial.println(ssid);
-      delay(500);
-    }
-
-    if(messageQueue != NULL) {
-      NetMessageOut* msg = NULL;
-      while(xQueueReceive( messageQueue, &msg, 0 )) { // returns true if there's any messages
-        if(msg != NULL && msg != nullptr) {
-          makeNetworkPacket(msg);
-          delete msg;
+        if (wStatus != WL_CONNECTED && wStatus != WL_IDLE_STATUS) {
+            WiFi.begin(ssid, password);
+            Serial.print("Current status: ");
+            Serial.print(wStatus);
+            Serial.print(". Reconnecting to ");
+            Serial.println(ssid);
+            delay(500);
         }
-      }
-    }
-    
-    if (wStatus == WL_CONNECTED) {
-      if (client.connected()) {
-        if (!buffer.isEmpty()) {
-          try {
-            Packet* p = buffer.pop();
-            if (p != nullptr) {
-              if (p->length > 0 && p->data != NULL && p->data != nullptr) {
-                Serial.println("Sending message.");
-                int sent = client.write(p->data, p->length);
-                Serial.print("Bytes sent: ");
-                Serial.println(sent);
-              }
-              if(p->data != NULL) {
-                delete[] p->data;
-              }
-              delete p;
+
+        if(messageQueue != NULL) {
+            NetMessageOut* msg = NULL;
+            while(xQueueReceive( messageQueue, &msg, 0 )) { // returns true if there's any messages
+                if(msg != NULL && msg != nullptr) {
+                    makeNetworkPacket(msg);
+                    delete msg;
+                }
             }
-          } catch (std::exception e) {
-            Serial.println(e.what());
-          }
         }
+        
+        if (wStatus == WL_CONNECTED) {
+            if (client.connected()) {
+                if (!buffer.isEmpty()) {
+                    try {
+                        Packet* p = buffer.pop();
+                        if (p != nullptr) {
+                            if (p->length > 0 && p->data != NULL && p->data != nullptr) {
+                                Serial.println("Sending message.");
+                                int sent = client.write(p->data, p->length);
+                                Serial.print("Bytes sent: ");
+                                Serial.println(sent);
+                            }
+                            if(p->data != NULL) {
+                                delete[] p->data;
+                            }
+                            delete p;
+                        }
+                    } catch (std::exception e) {
+                        Serial.println(e.what());
+                    }
+                }
 
-        try {
-          checkMessages();
-        } catch (std::exception e) {
-          Serial.println(e.what());
+                try {
+                    checkMessages();
+                } catch (std::exception e) {
+                    Serial.println(e.what());
+                }
+            } else {
+                if (client.connect(host, serverPort)) {
+                    Serial.print("Connected to ");
+                    Serial.println(host);
+                    client.setNoDelay(true); // consider setSync(true): flushes each write, slower but does not allocate temporary memory.
+                } else {
+                    Serial.print("Failed to connect to ");
+                    Serial.println(host);
+                }
+            }
         }
-      } else {
-        if (client.connect(host, serverPort)) {
-          Serial.print("Connected to ");
-          Serial.println(host);
-          client.setNoDelay(true); // consider setSync(true): flushes each write, slower but does not allocate temporary memory.
-        } else {
-          Serial.print("Failed to connect to ");
-          Serial.println(host);
-        }
-      }
+        CH::setRecordVideo(isConnected());
+    } catch (std::exception e) {
+        Serial.print("Exception in network handler: ");
+        Serial.println(e.what());
     }
-    CH::setRecordVideo(isConnected());
-  } catch (std::exception e) {
-    Serial.print("Exception in network handler: ");
-    Serial.println(e.what());
-  }
 }
 
 void pushNetMessage(NetMessageOut* msg) {
-  xQueueSend( messageQueue, &msg, xBlockTime );
+    xQueueSend( messageQueue, &msg, xBlockTime );
 }
 
 void makeNetworkPacket(NetMessageOut* msg) {
-  Packet* p = new Packet();
-  p->length = buf->messageOutToByteArray(p->data, msg);
-  buffer.enqueue(p);
+    Packet* p = new Packet();
+    p->length = buf->messageOutToByteArray(p->data, msg);
+    buffer.enqueue(p);
 }
 
 bool isConnected() {
-  return (wStatus == WL_CONNECTED && client.connected());
+    return (wStatus == WL_CONNECTED && client.connected());
 }
 
 void checkMessages() {
-  while (client.available()) {
-    int bytes = client.read(readBuf, readBufferSize);
-    if (bytes <= 0) {
-      break;
-    } else {
-      buf->insertBuffer(readBuf, bytes, true);
-      buf->checkMessages();
+    while (client.available()) {
+        int bytes = client.read(readBuf, readBufferSize);
+        if (bytes <= 0) {
+            break;
+        } else {
+            buf->insertBuffer(readBuf, bytes, true);
+            buf->checkMessages();
+        }
     }
-  }
-  NetMessageIn* msg = nullptr;
-  do {
-    msg = buf->popMessage();
-    if(msg != nullptr) {
-      processMessage(msg);
-    }
-    delete msg;
-  } while(msg != nullptr);
+    NetMessageIn* msg = nullptr;
+    do {
+        msg = buf->popMessage();
+        if(msg != nullptr) {
+            processMessage(msg);
+        }
+        delete msg;
+    } while(msg != nullptr);
 }
 
 void processMessage(NetMessageIn* msg) {
-  uint8_t type = msg->readVarInt();
-  Serial.println("Message received.");
-  Serial.printf("Length: %d, type: %d.\n", msg->getInternalBufferLength(), type);
-  switch(type) {
-    case 0:
-      std::string text = msg->readVarString();
-      Serial.printf("Message: %s\n", text.c_str());
-      break;
-  }
+    uint8_t type = msg->readVarInt();
+    Serial.println("Message received.");
+    Serial.printf("Length: %d, type: %d.\n", msg->getInternalBufferLength(), type);
+    switch(type) {
+        case 0:
+            std::string text = msg->readVarString();
+            Serial.printf("Message: %s\n", text.c_str());
+            break;
+    }
 }
 
 void startNetworkHandlerTask() {
-  messageQueue = xQueueCreate( 5, sizeof( NetMessageOut* ) );
-  xTaskCreateUniversal(networkHandlerTask, "NetworkTask", 16000, NULL, 0, &networkTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
+    messageQueue = xQueueCreate( 5, sizeof( NetMessageOut* ) );
+    xTaskCreateUniversal(networkHandlerTask, "NetworkTask", 16000, NULL, 0, &networkTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
 }
 
 void networkHandlerTask(void *pvParameters) {
-  while(true) {
-    handleNetworkStuff();
-    delay(1);
-  }
+    while(true) {
+        handleNetworkStuff();
+        delay(1);
+    }
 }
 
 } // namespace NH
