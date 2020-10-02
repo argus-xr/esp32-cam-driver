75d4621a869abaa59e0fa8b7b325df1910940f63
Dekker3D <dekker3d@gmail.com>
2020-10-02 15:25:51 +0200

    Replaced a naive stack implementation with the STL version.

--- ControllerDriver/lib/argus-utils/argus-netbuffer/BasicMessageProtocol/BasicMessageProtocol.cpp
+++ ControllerDriver/lib/argus-utils/argus-netbuffer/BasicMessageProtocol/BasicMessageProtocol.cpp
@@ -17,72 +17,59 @@ BasicMessageBuffer::~BasicMessageBuffer() {
 }
 
 void BasicMessageBuffer::checkMessages() {
-	if (messageListNum < 250) { // don't overflow the message array
-		int32_t pos = findByteSequence(startSequence, startSequenceLength, 0);
-		int32_t chop = -1;
-		if (pos == 0) {
-			bool posFound = false;
-			int32_t endpos = 1;
-			while (!posFound && endpos > 0) { // endpos < 0 means there's no more matching sequences.
-				endpos = findByteSequence(endSequence, endSequenceLength, endpos + 1);
-				if (endpos > 0 && getByteAt(endpos - 1) != '\\') { // check if the \ character is escaped, which would make the \0 not count.
-					posFound = true;
-				}
+	int32_t pos = findByteSequence(startSequence, startSequenceLength, 0);
+	int32_t chop = -1;
+	if (pos >= 0) {
+		bool posFound = false;
+		int32_t endpos = 1;
+		while (!posFound && endpos > 0) { // endpos < 0 means there's no more matching sequences.
+			endpos = findByteSequence(endSequence, endSequenceLength, endpos + 1);
+			if (endpos > 0 && getByteAt(endpos - 1) != '\\') { // check if the \ character is escaped, which would make the \0 not count.
+				posFound = true;
 			}
-			if (posFound) {
-				chop = endpos + endSequenceLength;
-				uint64_t length = 0; // length VarInt, used to verify that the entire message was received. Not used yet.
-				uint8_t varIntBytes = ArgusNetUtils::readVarInt(internalBuffer + startSequenceLength, length); // read a VarInt into length. The number of bytes used is stored in bytes.
-				int32_t messageLength = endpos - (startSequenceLength + varIntBytes); // endpos is the start of the end-sequence, so don't subtract endSequenceLength.
-				if (messageLength >= length) { // message with escaped characters is longer than or equal to expected length
-					uint8_t* nmbuf = new uint8_t[messageLength];
-					uint32_t offset = 0;
-					uint32_t bufOffset = varIntBytes + startSequenceLength;
-					for (int i = 0; i < messageLength; ++i) {
-						if (internalBuffer[bufOffset + i] == escapeCharacter) {
-							if (i < messageLength - 1 && internalBuffer[bufOffset + i + 1] == escapeCharacter) {
-								nmbuf[i - offset] = escapeCharacter;
-								offset += 1;
-								i++; // skip one step because we already covered it.
-							}
+		}
+		if (posFound) {
+			chop = endpos + endSequenceLength;
+			uint64_t length = 0; // length VarInt, used to verify that the entire message was received. Not used yet.
+			uint8_t varIntBytes = ArgusNetUtils::readVarInt(internalBuffer + startSequenceLength, length); // read a VarInt into length. The number of bytes used is stored in bytes.
+			int32_t messageLength = endpos - (startSequenceLength + varIntBytes) - pos; // endpos is the start of the end-sequence, so don't subtract endSequenceLength.
+			if (messageLength >= length) { // message with escaped characters is longer than or equal to expected length
+				uint8_t* nmbuf = new uint8_t[messageLength];
+				uint32_t offset = 0;
+				uint32_t bufOffset = varIntBytes + startSequenceLength;
+				for (int i = 0; i < messageLength; ++i) {
+					if (internalBuffer[bufOffset + i] == escapeCharacter) {
+						if (i < messageLength - 1 && internalBuffer[bufOffset + i + pos + 1] == escapeCharacter) {
+							nmbuf[i - offset] = escapeCharacter;
+							offset += 1;
+							i++; // skip one step because we already covered it.
 						}
-						nmbuf[i - offset] = internalBuffer[bufOffset + i];
 					}
-					messageLength -= offset; // actual message length without escapes.
-					if (messageLength == length) {
-						NetMessageIn* newMessage = new NetMessageIn(nmbuf, messageLength, myMalloc, myFree);
-						if (messageList == nullptr || messageListNum >= messageListMax) {
-							resizeMessageList(messageListMax + 10);
-						}
-						messageList[messageListNum] = newMessage;
-						messageListNum += 1;
-					}
+					nmbuf[i - offset] = internalBuffer[bufOffset + i];
+				}
+				messageLength -= offset; // actual message length without escapes.
+				if (messageLength == length) {
+					NetMessageIn* newMessage = new NetMessageIn(nmbuf, messageLength, myMalloc, myFree);
+					messageList.push(newMessage);
 				}
 			}
 		}
-		else if (pos > 0) {
-			chop = pos;
-		}
-		else if (internalBufferContentSize > 100000) { // there's 100000 bytes worth of garbage in here that won't ever match the protocol, ditch it!
-			chop = internalBufferContentSize - startSequenceLength; // there could be a partial valid delimiter at the end, so don't delete everything.
-		}
-		if (chop > 0) {
-			removeStartOfBuffer(chop);
-		}
+	}
+	else if (internalBufferContentSize > 100000) { // there's 100000 bytes worth of garbage in here that won't ever match the protocol, ditch it!
+		chop = internalBufferContentSize - startSequenceLength; // there could be a partial valid delimiter at the end, so don't delete everything.
+	}
+	if (chop > 0) {
+		removeStartOfBuffer(chop);
 	}
 }
 
 NetMessageIn* BasicMessageBuffer::popMessage() {
-	if (messageList != nullptr && messageListPos < messageListNum) {
-		NetMessageIn* ret = messageList[messageListPos];
-		messageList[messageListPos] = nullptr;
-		messageListPos += 1;
-		if (messageListPos >= messageListMax) { // reached the end of the list
-			resizeMessageList(0);
-		}
-		return ret;
+	if (messageList.empty()) {
+		return nullptr;
 	}
-	return nullptr;
+	NetMessageIn* ret = messageList.top();
+	messageList.pop();
+	return ret;
 }
 
 OutPacket* BasicMessageBuffer::messageToOutPacket(NetMessageOut* msg) {
@@ -145,28 +132,4 @@ OutPacket* BasicMessageBuffer::messageToOutPacket(NetMessageOut* msg) {
 	OutPacket* p = new OutPacket(outBuf, outBufSize, myFree);
 
 	return p;
-}
-
-void BasicMessageBuffer::resizeMessageList(uint8_t size) {
-	if (size <= 0) {
-		size == 5;
-		/*if (messageList) {
-			delete[] messageList;
-			messageList = nullptr;
-		}*/
-	}
-	//else {
-		if (size < messageListNum + 5) {
-			size = messageListNum + 5; // don't drop messages
-		}
-		NetMessageIn** tmp = messageList;
-		messageList = new NetMessageIn *[size];
-		if (tmp) {
-			for (int i = 0; i < messageListNum - messageListPos; i++) {
-				messageList[i-messageListPos] = tmp[i];
-			}
-			//std::memcpy(messageList, tmp + messageListPos, messageListNum - messageListPos);
-			delete[] tmp;
-		}
-	//}
 }
--- ControllerDriver/lib/argus-utils/argus-netbuffer/BasicMessageProtocol/BasicMessageProtocol.h
+++ ControllerDriver/lib/argus-utils/argus-netbuffer/BasicMessageProtocol/BasicMessageProtocol.h
@@ -2,6 +2,7 @@
 #define BASICMESSAGEPROTOCOL_H
 
 #include "../netbuffer.h"
+#include <stack>
 
 class BasicMessageBuffer : public NetBuffer {
 public:
@@ -18,11 +19,7 @@ public:
 	const uint32_t endSequenceLength = 2;
 	const uint8_t escapeCharacter = '\\';
 protected:
-	NetMessageIn** messageList = nullptr;
-	uint8_t messageListNum = 0;
-	uint8_t messageListMax = 5;
-	uint8_t messageListPos = 0;
-	void resizeMessageList(uint8_t size);
+	std::stack<NetMessageIn*> messageList;
 };
 
 #endif // BASICMESSAGEPROTOCOL_H
